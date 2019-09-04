db856b9d7233d83705badcb2e58f438fdbf643c7
Jakub Z <jakzale@gmail.com>
2019-09-04 15:24:21 +0100

    Clean ArrayManipulation

--- src/ArrayManipulation.hs
+++ src/ArrayManipulation.hs
@@ -1,15 +1,31 @@
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
-
 module ArrayManipulation where
 
-import qualified Data.Vector.Unboxed as V
-import qualified Data.Vector.Unboxed.Mutable as M
 import Control.Monad
 import Control.Monad.ST
 import Data.List
+import Data.Function
 import Debug.Trace
 import System.Environment
 import System.IO
+
+parseInt :: String -> Int
+parseInt = read
+
+parseInts :: String -> [Int]
+parseInts = map parseInt . words
+
+parse2I :: String -> (Int, Int)
+parse2I str = (a, b)
+  where
+    [a, b] = parseInts str
+
+parse3I :: String -> (Int, Int, Int)
+parse3I str = (a, b, c)
+  where
+    [a, b, c] = parseInts str
+
+getNLines :: Int -> IO [String]
+getNLines n = replicateM n getLine
 
 -- Complete the arrayManipulation function below.
 arrayManipulation :: Int -> [(Int, Int, Int)] -> Int
@@ -20,32 +36,19 @@ arrayManipulation n queries =
     calcChanges ((a, b, k):rest) = (a, k):(b + 1, -k):calcChanges rest
     changes = filter ((<=n) . fst) (calcChanges queries)
     orderedChanges = sortOn fst changes
-    groupedChanges = groupBy (\ a b -> (fst a) == (fst b)) orderedChanges
+    groupedChanges = groupBy ((==) `on` fst) orderedChanges
     summedGroupedChanges = map (sum . (map snd)) groupedChanges
     values = scanl' (+) 0 summedGroupedChanges
 
-readMultipleLinesAsStringArray :: Int -> IO [String]
-readMultipleLinesAsStringArray 0 = return []
-readMultipleLinesAsStringArray n = do
-    line <- getLine
-    rest <- readMultipleLinesAsStringArray(n - 1)
-    return (line : rest)
 
 main :: IO()
 main = do
     stdout <- getEnv "OUTPUT_PATH"
     fptr <- openFile stdout WriteMode
 
-    nmTemp <- getLine
-    let nm = words nmTemp
+    (n, m) <- parse2I <$> getLine
+    tuples <- fmap parse3I <$> getNLines m
 
-    let n = read (nm !! 0) :: Int
-
-    let m = read (nm !! 1) :: Int
-
-    queriesTemp <- readMultipleLinesAsStringArray m
-    let queries = Data.List.map (\x -> Data.List.map (read :: String -> Int) . words $ x) queriesTemp
-    let tuples = Data.List.map (\[x, y, z] -> (x, y, z)) queries
     let result = arrayManipulation n tuples
 
     hPutStrLn fptr $ show result
