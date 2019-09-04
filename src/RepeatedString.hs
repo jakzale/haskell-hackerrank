2227395d57405665b677441b91c30b42e32f2a8f
Jakub Z <jakzale@gmail.com>
2019-09-04 15:48:09 +0100

    Clean RepeatedString

--- src/RepeatedString.hs
+++ src/RepeatedString.hs
@@ -1,13 +1,6 @@
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
-
 module RepeatedString where
 
-import Control.Monad
-import Data.Array
-import Data.Bits
 import Data.List
-import Data.List.Split
-import Data.Set
 import Debug.Trace
 import System.Environment
 import System.IO
